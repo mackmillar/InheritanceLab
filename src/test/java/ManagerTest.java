0e3f0826629b4d4838f0b2f7e6bc00779a7511b9
Alex Dodd <alexdodd@gmail.com>
2021-02-10 15:34:25 +0000

    manager test written and passed

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,2 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.management.Manager;
+
+import static org.junit.Assert.assertEquals;
+
 public class ManagerTest {
+
+    Manager manager;
+
+    @Before
+    public void before(){
+        manager = new Manager("Joe", "HY645327D", 25000.00, "HR");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Joe", manager.getName());
+    }
+
+    @Test
+    public void hasNi(){
+        assertEquals("HY645327D", manager.getNi());
+    }
+
+    @Test
+    public void hasSalary(){
+        assertEquals(25000.00, manager.getSalary(), 0.001);
+    }
+
+    @Test
+    public void hasDeptName(){
+        assertEquals("HR", manager.getDeptName());
+    }
+
 }
