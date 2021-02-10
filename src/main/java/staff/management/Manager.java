5077c287adea2d06919c4057936044e2c5f057ab
Alex Dodd <alexdodd@gmail.com>
2021-02-10 15:36:05 +0000

    created a manager class inheriting and its own deptName getter

--- src/main/java/staff/management/Manager.java
+++ src/main/java/staff/management/Manager.java
@@ -1,4 +1,19 @@
 package staff.management;
 
-public class Manager {
+import staff.Employee;
+
+public class Manager extends Employee {
+
+    private String deptName;
+
+    public Manager(String name, String ni, Double salary, String deptName){
+        super(name, ni, salary);
+        this.deptName = deptName;
+    }
+
+    public String getDeptName(){
+        return this.deptName;
+    }
+
+
 }
